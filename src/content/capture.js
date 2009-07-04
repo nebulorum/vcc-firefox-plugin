d2c5af0a33c0aa02eb226052a7b7b7a303a6615c
Thomas Santana <mailleux@gmail.com>
2009-07-04 00:46:12 +0000

    Fixed to Version 3.5 of Firefox and added mapping of D&DInsider ID on the XML sent.

--- src/content/capture.js
+++ src/content/capture.js
@@ -25,27 +25,46 @@ function dndiGetHTTPRequest() {
 }
 
 function dndiHome(event) {
-   var url="http://ww2.wizards.com/dnd/insider/database.aspx"
+   var url="http://www.wizards.com/dndinsider/compendium/database.aspx"
    window._content.document.location  = url
+}
+
+function dndiGetID(url) {
+  var re= new RegExp("id=([0-9]+)")
+  var match=re.exec(url)
+  if(match && match.length == 2)
+    return  match[1]
+  else  
+    return null
 }
 
 function dndiDoCapture(url) { 
   var data=window.content.document.getElementById("detail")
+  var id = dndiGetID(window.content.document.location)
+  
+  if(id == null) {
+  	alert("This page URL does not contain and ID parameter. Please make sure you are viewing a D&D Insider result page.")
+  	return 0;
+  }
   if(data==null) {
     alert("The page does not seem to contain and capturable data, must be a D&D Insider result page.")
-	return 0;
+	  return 0;
   }
+
   var xmlHttp=dndiGetHTTPRequest();
 
   var serializer = new XMLSerializer();
-  data=serializer.serializeToString(window.content.document.getElementById("detail"));
+  
+	data= window.content.document.getElementById("detail").cloneNode(true);
+	data.setAttribute("id",id)
+	data=serializer.serializeToString(data);
 
   xmlHttp.onreadystatechange  = function()
     { 
          if(xmlHttp.readyState  == 4)
          {
               if(xmlHttp.status  == 200) 
-                  alert(xmlHttp.responseText) 
+                  alert("Virtual Combat Cards replied: "+xmlHttp.responseText) 
               else 
                  alert("Sending to "+url+" failed with error code: " + xmlHttp.status);
          }
@@ -64,12 +83,11 @@ function dndiDoCapture(url) {
   //xmlHttp.channel.cancel(true);
 }
 
-
 function dndiCapture(event) {
 	var url = document.getElementById("dndiVccUrl").value;
 	if(url==null || url =="") {
 		alert("You must specify the Remote URL parameter.");
 	} else { 
 		dndiDoCapture(url);
-    }
+  }
 }
