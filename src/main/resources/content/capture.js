c4604dca1d995c644086b976c761e21bbf047de8
Thomas Santana <mailleux@gmail.com>
2010-09-02 03:28:39 +0000

    Implemented basic auto-capture feature. Clean the old XUL objects. Externalized most of the strings. Added style-sheets for bars. D&DI Capture toolbar has been removed.

--- src/main/resources/content/capture.js
+++ src/main/resources/content/capture.js
@@ -16,213 +16,250 @@
  */
 //$Id: capture.js 474 2009-12-13 15:12:41Z mailleux@gmail.com $
 
-function dndiGetHTTPRequest() {
-    try {
-        return new XMLHttpRequest();
-    } catch(e) {
-    }
-    try {
-        return new ActiveXObject("Msxml2.XMLHTTP");
-    } catch (e) {
-    }
-    try {
-        return new ActiveXObject("Microsoft.XMLHTTP");
-    } catch (e) {
-    }
-    alert("XMLHttpRequest not supported");
-    return null;
-}
-
-function dndiHome(event) {
-    var url = "http://www.wizards.com/dndinsider/compendium/database.aspx";
-    window._content.document.location = url;
-}
-
-function dndiGetID(url) {
-    var re = new RegExp("id=([0-9]+)");
-    var match = re.exec(url);
-    if (match && match.length == 2)
-        return  match[1];
-    else
-        return null;
-}
-
-/*
- * This function will try to find the DIV with ID="detail", if it fails it 
- * will look for Iframes with that div.
- */
-function dndiFindSection() {
-    var doc = window.content.document;
-
-    while (doc != null) {
-        var data = doc.getElementById("detail");
-        var id = dndiGetID(doc.location);
-
-        if (id != null && data != null) {
-            var ndata = data.cloneNode(true);
-            ndata.setAttribute("id", id);
-            return ndata;
-        }
-
-        // Go down IFrame
-        var res = doc.evaluate("//iframe", doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-
-        if (res != null && res.singleNodeValue != null)
-            doc = res.singleNodeValue.contentWindow.document;
-        else
-            doc = null;
-    }
-    return null
-}
-
-
-function dndiDoCapture(url, callback) {
-    var data = dndiFindSection();
-
-    if (data == null) {
-        alert("The page does not seem to contain and capturable data, must be a D&D Insider result page.");
-        return 0;
-    }
-
-    var xmlHttp = dndiGetHTTPRequest();
-    var serializer = new XMLSerializer();
-    data = serializer.serializeToString(data);
-
-    xmlHttp.onreadystatechange = function() {
-        callback(xmlHttp);
-    };
-
-
-    // Open a connection to the server
-    xmlHttp.open("POST", url, true);
-
-    // Tell the server you're sending it XML
-    xmlHttp.setRequestHeader("Content-Type", "text/xml");
-    xmlHttp.setRequestHeader("Connection", "close");
-
-    // Send the request
-    xmlHttp.send(data);
-}
-
-function dndiCapture(event) {
-    var url = document.getElementById("dndiVccUrl").value;
-    if (url == null || url == "") {
-        alert("You must specify the Remote URL parameter.");
-    } else {
-        dndiDoCapture(url, function(xmlHttp) {
-            if (xmlHttp.readyState == 4) {
-                if (xmlHttp.status == 200)
-                    alert("Virtual Combat Cards replied: " + xmlHttp.responseText);
-                else
-                    alert("Sending to " + url + " failed with error code: " + xmlHttp.status);
-            }
-        });
-    }
-}
-
-function dndiEvalStringOnTarget(tgtWin, evalString) {
-    var contentWin = null;
-    var win = tgtWin.content.wrappedJSObject;
-    var sb = new Components.utils.Sandbox(win);
-    sb.window = win;
-    return Components.utils.evalInSandbox("with(window){" + evalString + "}", sb);
-}
-
-function dndiCanAdvance() {
-    var button = window.content.document.getElementById("GB_middle");
-    if (button && button.children[2].getAttribute("class").indexOf("disabled") == -1) return true;
-    else return false;
-}
-
-function dndiResultSetNextEntry(lsnr) {
-    if (!dndiCanAdvance) {
-        alert("Last entry reached.");
-        if (lsnr) {
-            lsnr.stop = true;
-            lsnr.unregister();
-        }
-    } else {
+var dndiCapture = {
+    getHTTPRequest: function() {
         try {
-            dndiEvalStringOnTarget(window, "GB_CURRENT.switchNext();");
+            return new XMLHttpRequest();
         } catch(e) {
-            if (e.name == "ReferenceError") alert("You must be view multiple compendium entries to use this feature.");
-            else if (e.name == "TypeError") alert("Select on of the entries to start the mass capture.");
-            else alert("Unexpected error looking for GreyBar: " + e);
-            if (lsnr) lsnr.stop = true;
         }
-    }
-}
-
-var dndiListener = {
-    QueryInterface: function (iid) {
-        if (iid.equals(Components.interfaces.nsIWebProgressListener) || iid.equals(Components.interfaces.nsISupportsWeakReference))
-            return this;
-        throw Components.results.NS_NOINTERFACE;
+        try {
+            return new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+        }
+        try {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (e) {
+        }
+        alert("XMLHttpRequest not supported");
+        return null;
     },
-    onStateChange: function (webProgress, request, stateFlags, status) {
-        if (stateFlags == 0x80010) {
-            this.unregister();
-            if (!this.stop || this.nextMove != null) {
-                this.register();
-                this.nextMove(this);
+
+    gotoCompendium: function(event) {
+        var url = "http://www.wizards.com/dndinsider/compendium/database.aspx";
+        window._content.document.location = url;
+    },
+
+    getEntryID: function (url) {
+        var re = new RegExp("id=([0-9]+)");
+        var match = re.exec(url);
+        if (match && match.length == 2)
+            return  match[1];
+        else
+            return null;
+    },
+
+    /*
+     * This function will try to find the DIV with ID="detail", if it fails it
+     * will look for Iframes with that div.
+     */
+    findSection: function() {
+        var doc = window.content.document;
+
+        while (doc != null) {
+            var data = doc.getElementById("detail");
+            var id = this.getEntryID(doc.location);
+
+            if (id != null && data != null) {
+                var ndata = data.cloneNode(true);
+                ndata.setAttribute("id", id);
+                return ndata;
             }
+
+            // Go down IFrame
+            var res = doc.evaluate("//iframe", doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+
+            if (res != null && res.singleNodeValue != null)
+                doc = res.singleNodeValue.contentWindow.document;
+            else
+                doc = null;
         }
-
-    } ,
-    stop: false ,
-    unregister: function() {
-        //alert("Unregistered self");
-        var myBrowser = window.document.getElementById("content");
-        var wp = myBrowser.webNavigation.QueryInterface(Components.interfaces.nsIWebProgress);
-        wp.removeProgressListener(this);
+        return null;
     },
-    register: function() {
-        //alert("Registered self");
-        var myBrowser = window.document.getElementById("content");
-        var wp = myBrowser.webNavigation.QueryInterface(Components.interfaces.nsIWebProgress);
-        wp.addProgressListener(dndiListener, Components.interfaces.nsIWebProgress.NOTIFY_STATE_WINDOW);
-    }
-};
 
-function dndiStepAndAdvance(move) {
 
-    //const nsIWebNavigation          = Components.interfaces.nsIWebNavigation;
-    //const nsIWebProgress            = Components.interfaces.nsIWebProgress;
-    //const nsIWebProgressListener    = Components.interfaces.nsIWebProgressListener;
-    //const nsISupportsWeakReference  = Components.interfaces.nsISupportsWeakReference;
-    dndiListener.nextMove = move;
-    dndiListener.register();
-    dndiListener.nextMove(dndiListener);
-}
+    // Callback is only used if we have success, otherwise we stop everything
+    sendCapture: function(callback) {
+        var url = this.getVCCURL() + "/capture?reply=plugin-text"
+        var data = this.findSection();
 
-function dndiCaptureAndAdvance(lsnr) {
-    var url = document.getElementById("dndiVccUrl").value;
-    if (url == null || url == "") {
-        alert("You must specify the Remote URL parameter.");
-        lsnr.stop = true;
-    } else {
-        dndiDoCapture(url, function(xmlHttp) {
+        if (data == null) {
+            alert("The page does not seem to contain and capturable data, must be a D&D Insider result page.");
+            return 0;
+        }
+        var xmlHttp = this.getHTTPRequest();
+        var serializer = new XMLSerializer();
+        data = serializer.serializeToString(data);
+
+        xmlHttp.onreadystatechange = function() {
             if (xmlHttp.readyState == 4) {
-                if (xmlHttp.status != 200) {
-                    alert("Sending to " + url + " failed with error code: " + xmlHttp.status);
-                    lnsr.stop = true;
-                    lnsr.unregister();
+                if (xmlHttp.status == 200) {
+                    var fields = xmlHttp.responseText.split(":");
+                    dndiCapture.addResult(fields[1], fields[0]);
+                    if (callback != null) callback(xmlHttp);
                 } else {
-                    dndiResultSetNextEntry(lsnr);
+                    dndiCapture.addResult("Failed to contact VCC", "Failed");
+                    dndiCapture.AutoCapture.stopAutomation();
                 }
             }
-        });
+        };
+        // Open a connection to the server
+        xmlHttp.open("POST", url, true);
+
+        // Tell the server you're sending it XML
+        xmlHttp.setRequestHeader("Content-Type", "text/xml");
+        xmlHttp.setRequestHeader("Connection", "close");
+
+        // Send the request
+        xmlHttp.send(data);
+    },
+
+
+    withGetReply: function(path, okCallback, errorCallback) {
+        var url = this.getVCCURL()
+        var xmlHttp = this.getHTTPRequest();
+
+        xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState == 4) {
+                if (xmlHttp.status == 200) {
+                    if (okCallback != null) okCallback(xmlHttp.responseText)
+                } else {
+                    if (errorCallback != null) errorCallback(xmlHttp.status, xmlHttp.responseText)
+                }
+            }
+        }
+        xmlHttp.open("GET", url + path, true);
+        xmlHttp.send(null);
+    },
+
+
+    addResult: function(name, status) {
+        var theList = document.getElementById('dndiCaptureResult');
+        var row = document.createElement('listitem');
+        var cell = document.createElement('listcell');
+        cell.setAttribute('label', name);
+        row.appendChild(cell);
+
+        cell = document.createElement('listcell');
+        cell.setAttribute('label', status);
+        row.appendChild(cell);
+
+        theList.appendChild(row);
+    },
+    getVCCURL: function() {
+        var prefService = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+        var branch = prefService.getBranch("dndicapture.");
+        if (branch.prefHasUserValue("vcc.url"))
+            return branch.getCharPref("vcc.url");
+        else {
+            branch.setCharPref("vcc.url", "http://127.0.0.1:4143");
+            return branch.getCharPref("vcc.url");
+        }
+    },
+    doCapture: function (event) {
+        this.sendCapture(null);
+    },
+
+    AutoCapture: {
+        //Callback information for registering
+        QueryInterface: function (iid) {
+            if (iid.equals(Components.interfaces.nsIWebProgressListener) || iid.equals(Components.interfaces.nsISupportsWeakReference))
+                return this;
+            throw Components.results.NS_NOINTERFACE;
+        },
+        onStateChange: function (webProgress, request, stateFlags, status) {
+            if (stateFlags == 0x80010) {
+                if (this.running) {
+                    dndiCapture.sendCapture(function(xhr) {
+                        var myself = dndiCapture.AutoCapture;
+                        if (xhr.responseText.match("^FATAL:") == "FATAL:") {
+                            myself.stopAutomation();
+                        } else if (myself.autoAdvance) myself.advanceToNextEntry();
+                    });
+                }
+            }
+
+        },
+        running: false,
+        autoAdvance: false,
+        contentWindow: null,
+        unregister: function() {
+            if (this.contentWindow != null) {
+                var wp = this.contentWindow.webNavigation.QueryInterface(Components.interfaces.nsIWebProgress);
+                wp.removeProgressListener(this);
+            }
+        },
+        register: function() {
+            //alert("Registered self");
+            var wp = this.contentWindow.webNavigation.QueryInterface(Components.interfaces.nsIWebProgress);
+            wp.addProgressListener(this, Components.interfaces.nsIWebProgress.NOTIFY_STATE_WINDOW);
+        },
+        startAutomation: function(autoNext) {
+            var mainWindow = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+                    .getInterface(Components.interfaces.nsIWebNavigation)
+                    .QueryInterface(Components.interfaces.nsIDocShellTreeItem)
+                    .rootTreeItem
+                    .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+                    .getInterface(Components.interfaces.nsIDOMWindow);
+            this.contentWindow = mainWindow.document.getElementById("content");
+            this.running = true;
+            this.autoAdvance = autoNext;
+            this.register();
+        },
+        stopAutomation: function() {
+            this.running = false;
+            this.unregister();
+            alert("Stop pressed:" + this.running);
+        },
+        advanceToNextEntry: function() {
+            var button = this.contentWindow.contentDocument.getElementById("GB_middle");
+            if (button && button.children[2].getAttribute("class").indexOf("disabled") == -1) {
+                try {
+                    dndiCapture.executeJSOnTarget(this.contentWindow.contentWindow, "GB_CURRENT.switchNext();");
+                } catch(e) {
+                    if (e.name == "ReferenceError") alert("You must be view multiple compendium entries to use this feature.");
+                    else if (e.name == "TypeError") alert("Select on of the entries to start the mass capture.");
+                    else alert("Unexpected error looking for GreyBar: " + e);
+                    if (lsnr) lsnr.stop = true;
+                }
+            }
+        }
+    },
+
+    executeJSOnTarget: function(tgtWin, evalString) {
+        var win = tgtWin.content.wrappedJSObject;
+        var sb = new Components.utils.Sandbox(win);
+        sb.window = win;
+        return Components.utils.evalInSandbox("with(window){" + evalString + "}", sb);
+    },
+
+    confirmAutomation: function() {
+        var ps = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
+        var rv = ps.confirmEx(window, "Experimental Feature",
+                "This is a experimental feature which will add automation to your browser. " +
+                        "You should use this only if you are not doing anything important. " +
+                        "You may have to close the browser if things don't work correctly.\n\nDo you want to activate?",
+                ps.BUTTON_TITLE_IS_STRING * ps.BUTTON_POS_0 +
+                        ps.BUTTON_TITLE_IS_STRING * ps.BUTTON_POS_1,
+                "Yes", "No", null, null, {});
+        return rv == 0;
+    },
+
+    startAutoCapture: function(event, autoNext) {
+        if (this.confirmAutomation()) {
+            // Go ahead
+            this.withGetReply("/capture?has=auto", function(response) {
+                if (response == "true") {
+                    dndiCapture.AutoCapture.startAutomation(autoNext);
+                } else {
+                    alert("Current Virtual Combat Cards does not support this automation, use version 1.4.0 or higher");
+                }
+            },
+                    function(code, response) {
+                        alert("Failed to contact Virtual Combat Cards, please start it.");
+                    })
+        }
+    },
+    stopAutoCapture: function(event) {
+        this.AutoCapture.stopAutomation();
     }
 }
-
-/*
- var ps = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
- .getService(Components.interfaces.nsIPromptService);
- var rv = ps.confirmEx(window, "Experimental Feature", "This is a experimental feature which will assume control of the browser. You should use this only if you are not doing anything important. You may have to close the browser if things don't work correctly.\n\nProceed?",
- ps.BUTTON_TITLE_IS_STRING * ps.BUTTON_POS_0 +
- ps.BUTTON_TITLE_IS_STRING * ps.BUTTON_POS_1,
- "Yes", "No", null, null, {});
- if(rv == 0) { // Go ahead
- }
- */
-//dndiStepAndAdvance(dndiCaptureAndAdvance)
